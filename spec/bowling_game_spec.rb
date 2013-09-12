cff45343cf4ad5c459853bbdbf665d50e09bd17f
Nik Daftary <nikdaftary@gmail.com>
2013-09-12 14:02:39 -0500

    failing test expecting 20 score for 1 roll, getting 0å

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -5,7 +5,7 @@ require_relative '../bowling_game.rb'
 
 
 describe BowlingGame do
-  describe '.initialize' do
+  describe '.initialize' do # this all happens within the initialize class method
     it 'sets @rolls to an empty array' do
       game = BowlingGame.new
 
@@ -13,7 +13,9 @@ describe BowlingGame do
     end
   end
 
-  describe "#score" do 
+  #---------------- SCORE METHOD TESTS ---------------------------------#
+
+  describe "#score" do # this happens within the score instance method. 
   	it 'scores the game as 0 when only gutter balls are thrown' do
   		game = BowlingGame.new
   		20.times { game.roll(0) }
@@ -21,7 +23,21 @@ describe BowlingGame do
   		expect(game.score).to eq(0)
   	end
 
-  end
+
+  	#creating a new expectation when one pin is knocked down vs. gutter ball (0 pins)
+  	it 'scores the game as 20 when one pin is knocked down on each roll' do
+  		game = BowlingGame.new
+  		20.times { game.roll(1) }
+
+  		expect(game.score).to eq(20)
+  	end
+
+  	
+  end  #ending the score method tests
+
+  #---------------- ROLL METHOD TESTS ---------------------------------#
+
+
 
 
 
